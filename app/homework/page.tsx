d1a4a726e5a53923903598732dec7d992f7d3841
[Muahmmad Saadullah] <msaadullah2022@gmail.com>
2024-11-12 00:59:15 +0500

    initial commit

---
+++ app/homework/page.tsx
@@ -0,0 +1,15 @@
+export default function Homework(){
+    return(
+        <header>
+            <nav className="flex w-full h-40 bg-gray-600">
+                <div>
+                
+                </div>
+                <div>
+
+                </div>
+                <div></div>
+            </nav>
+        </header>
+    )
+}
---
+++ app/page copy.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home2() {
+  return (
+    <div className="bg-white">
+      <h1 className="text-purple-800 font-bold text-7xl">Tailwind CSS</h1>
+      <p className="text-red-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem quas odio accusamus officia ipsa consequatur, ratione itaque quo numquam modi repudiandae tempora ab accusantium quam nam, ullam voluptatibus repellat quasi.</p>
+    </div>
+  );
+}
